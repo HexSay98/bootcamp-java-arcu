57371ff0712543d71b693902bbb186aea9072eb2
HexSay98 <arcudi68@gmail.com>
2022-03-24 15:46:42 -0300

    Clase 7 act
    -clase 7 al dia

--- app-web/src/main/java/clase7/Carrito.java
+++ app-web/src/main/java/clase7/Carrito.java
@@ -2,6 +2,83 @@ package clase7;
 
 public class Carrito {
 	
+	//atributos
+	Item[] items;
 	
-
+	//constructores
+	Carrito(){
+		this.items = new Item[0];
+	}
+	
+	void agregarCarrito(Articulo art) {
+		//agregar constructor a item
+		Item i = new Item(art,1);
+		Item[] nuevos = new Items[this.items.length + 1];
+		
+		//copiar los actuales al nuevo
+		
+		items[items.length] = i;//ojo se rmope
+		this.items = nuevos;
+	}
+	
+	void seguirComprando() {
+		System.out.println("Seguimos comprando.");
+	}
+	
+	void limpiar() {
+		this.items = new Item[0];
+	}
+	
+	void actualizar(Item[] itemsActualizados) {
+		this.items = itemsActualizados;
+		
+	}
+	
+	Double obtenerPrecio() {
+		//Como calculo?
+		
+		Double precioFinal = 0d;
+		
+		for(Item unItem : this.items ) {
+			precioFinal += unItem.articulo.precio;
+		}
+		return precioFinal;
+	}
+	
+	
+	void eliminarItem(Item itemAEliminar) {
+		for(Item item : this.items) {
+			if(existeItem(itemAEliminar.articulo.id)) {
+				//int pos = obtenePosicion(itemAEliminar.articulo.id);
+				//podemos crear un nuevo vector con un elemento menos de lo que tiene el original
+				Item[] nuevos = new Item[this.items.length - 1];
+				
+			}
+		}
+	}
+	
+	Integer obtenerCantidadItems() {
+		return this.items.length;
+	}
+	
+	
+	void copiarItems() {
+		Item[] nuevos = new Item[this.items.length - 1];
+		for(Item item : this.items) {
+			
+		}
+		this.items = nuevos;
+	}
+	
+	
+	boolean existeItem(Long idArticulo){
+		boolean existe = false;
+		for(Item item : this.items) {
+			if(item.articulo.id.equals(idArticulo)) {
+				existe = true;
+				break;
+			}
+		}
+		return existe;
+	}
 }
---
+++ app-web/src/main/java/clase7/TematikMain.java
@@ -0,0 +1,50 @@
+package clase7;
+
+public class TematikMain {
+
+	public static void main(String[] args) {
+		
+		String claveB = "batman";
+		
+		//quiero realizar una b usqueda
+		//1- crear un buscador= instanciar un objeto de la clase buscador
+		//2 - ejecutar el metodo buscar de la instancia creada
+		
+		Buscador miBuscador = new Buscador(claveB);
+		
+		//miBuscador.claveBusqueda = claveB;
+		
+		miBuscador.buscar();//f3
+		
+		//mostramos cuantos resultados encuentro
+		miBuscador.mostrarContidadResultados();
+		
+		if(miBuscador.hayResultados()) {
+			
+			//Como recorro los resultados?
+			
+			miBuscador.detalle();
+			
+			
+		}
+		//creo el carrito
+		Carrito carrito = new Carrito();
+		
+		
+		//ahora agrego los articulos impares al carrito
+		
+		if(miBuscador.hayResultados()) {
+			for(int i = 0; i < miBuscador.articulos.length; i++) {
+				if(i%2 != 0) {
+					
+				}
+			}
+		}
+		
+		
+		
+	}
+
+
+
+}
