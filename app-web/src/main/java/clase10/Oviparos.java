ee298395086aa1a7a11b4b850a37d7dde6a61d3e
HexSay98 <arcudi68@gmail.com>
2022-04-08 18:59:26 -0300

    kkk

--- app-web/src/main/java/clase10/Oviparos.java
+++ app-web/src/main/java/clase10/Oviparos.java
@@ -4,6 +4,11 @@ public class Oviparos extends Animal{
 	
 	//tiene lo mismo que un animal
 	
+	public Oviparos(String color) {
+		super(color);
+		// TODO Auto-generated constructor stub
+	}
+
 	private Float tamanioHuevo;
 	
 	//nacen de una manera particular
